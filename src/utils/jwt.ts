7f5a1bc458d70e7882736976769c8d4d91a2a106
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-10 20:32:59 +0200

    feat: create multiple cookies, accessToken and refreshToken in attachCookiesToResponse

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,17 +3,40 @@ import "dotenv/config";
 import { IAttachCookies, ITokenUserPayload } from "../types/authInterfaces";
 
 export const createJWT = ({ payload }: ITokenUserPayload): string => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_LIFETIME,
-  });
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
   return token;
 };
 
 export const isTokenValid = (token: string) =>
   jwt.verify(token, process.env.JWT_SECRET);
 
-export const attachCookiesToResponse = ({ res, user }: IAttachCookies) => {
-  const token = createJWT({ payload: user });
+export const attachCookiesToResponse = ({
+  res,
+  user,
+  refreshToken,
+}: IAttachCookies) => {
+  const accessTokenJWT = createJWT({ payload: { user } });
+  const refreshTokenJWT = createJWT({ payload: { user, refreshToken } });
+
+  const oneDay = 1000 * 60 * 60 * 24;
+  // const fiveSeconds = 1000 * 5;
+  res.cookie("accessToken", accessTokenJWT, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    signed: true,
+    maxAge: 1000,
+  });
+
+  res.cookie("refreshToken", refreshTokenJWT, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === "production",
+    signed: true,
+  });
+};
+
+export const attachSingleCookieToResponse = ({ res, user }: IAttachCookies) => {
+  const token = createJWT({ payload: { user } });
   // const oneDay = 1000 * 60 * 60 * 24;
   const fiveSeconds = 1000 * 5;
   res.cookie("token", token, {
